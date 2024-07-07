ce9e7bfa82b261885c30cd4d451e9cc4b56ff880
Lexacus <aleserr894@gmail.com>
2024-07-07 10:17:27 +0200

    feat: post user, post exercise, get users, get logs

--- index.ts
+++ index.ts
@@ -10,16 +10,38 @@ const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI ?? "");
 
-const urlSchema = new Schema({
-  original_url: {
-    type: String,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+    },
   },
-  short_url: {
-    type: Number,
-  },
-});
+  { versionKey: false }
+);
 
-let urlInfo = model("urlInfo", urlSchema);
+const exerciseSchema = new Schema(
+  {
+    username: {
+      type: String,
+    },
+    ownerId: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    duration: {
+      type: Number,
+    },
+    date: {
+      type: String,
+    },
+  },
+  { versionKey: false }
+);
+
+let User = model("user", userSchema);
+let Exercise = model("exercise", exerciseSchema);
 
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
@@ -28,48 +50,63 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_, res: Response) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/shorturl", async (req: Request, res: Response) => {
-  if (!isValidUrl(req.body.url)) {
-    res.json({ error: "invalid url" });
+app
+  .post("/api/users", async (req: Request, res: Response) => {
+    if (!req.body.username) {
+      return res.json({ error: "Invalid username" });
+    }
+    const createdUser = await User.create({ username: req.body.username });
+    return res.json(createdUser);
+  })
+  .get("/api/users", async (_, res: Response) => {
+    const allUsers = await User.find({});
+    return res.send(allUsers);
+  });
+
+app.post("/api/users/:_id/exercises", async (req: Request, res: Response) => {
+  const exerciseOwner = await User.findOne({ _id: req.params._id });
+  if (!exerciseOwner) {
+    return res.json({ error: "No user found with provided id." });
   }
-  const urlToFind = await urlInfo
-    .findOne({ original_url: req.body.url })
-    .select({ original_url: 1, short_url: 1 });
-  if (!urlToFind) {
-    const docNumber = await urlInfo.countDocuments();
-    const newUrl = await urlInfo.create({
-      original_url: req.body.url,
-      short_url: docNumber,
-    });
-    return res.json({
-      original_url: newUrl.toObject().original_url,
-      short_url: newUrl.toObject().short_url,
-    });
-  }
-  res.json({
-    original_url: urlToFind.toObject().original_url,
-    short_url: urlToFind.toObject().short_url,
+  const parsedDate = !req.body.date ? new Date() : new Date(req.body.date);
+  const createdExercise = await Exercise.create({
+    ...exerciseOwner,
+    ...req.body,
+    ownerId: exerciseOwner.id,
+    date: parsedDate.toDateString(),
+  });
+  const { _id, ownerId, ...rest } = createdExercise.toObject();
+  return res.json({
+    ...exerciseOwner.toObject(),
+    ...rest,
   });
 });
 
-app.get("/api/shorturl/:shortUrl", async (req: Request, res: Response) => {
-  const urlToFind = await urlInfo.findOne({ short_url: req.params.shortUrl });
-  if (!urlToFind) {
-    return res.json({ notFound: "short url not found" });
+app.get("/api/users/:_id/logs", async (req: Request, res: Response) => {
+  const user = await User.findOne({ _id: req.params._id });
+  if (!user) {
+    return res.json({ error: "No user found with provided id." });
   }
-  const urlToRedirect = urlToFind.toObject().original_url;
-  if (!urlToRedirect) {
-    return res.json({ notValid: "recovered url is not valid" });
-  }
-  res.redirect(urlToRedirect);
+  const userExercises = await Exercise.find({ ownerId: user.id });
+  const userLog = userExercises.map(({ description, duration, date }) => ({
+    description,
+    duration,
+    date,
+  }));
+  return res.json({
+    ...user.toObject(),
+    count: userExercises.length,
+    log: userLog,
+  });
 });
 
 app.get("/api/reset", async (req: Request, res: Response) => {
-  await urlInfo.deleteMany({});
+  await User.deleteMany({});
+  await Exercise.deleteMany({});
   res.json({ reset: "reset" });
 });
 
